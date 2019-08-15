0d77537f958c5c46a158a260bfe3a090c620a25b
bruno <bruno.crosier@gmail.com>
2019-08-15 20:40:03 +0100

    fix link

--- components/BelowFold.js
+++ components/BelowFold.js
@@ -74,7 +74,7 @@ const BelowFold = () => (
     <WaveSVG />
     <div id="below-fold-root">
       <div className="col left">
-        <Link href="https://github.com/brunocrosier/password.kiwi"><h4>FULLY OPEN SOURCE</h4></Link>
+        <Link href="https://github.com/brunocrosier/password.kiwi"><h4><a>FULLY OPEN SOURCE</a></h4></Link>
         <h1>100% secure</h1>
         <p>
           Every password on this page is created securely, randomly and
